daf5f6e6b3da0e2fbd9e2930aef4585dc055454a
enda-quantmatix <enda.carroll@quantmatix.com>
2024-06-07 15:13:06 +0100

    Fixed error with broker cash

--- backtesting/backtesting.py
+++ backtesting/backtesting.py
@@ -1246,6 +1246,7 @@ class Backtest:
             self._results["TotalEquity"]  = pd.Series(equity)
             self._results["BrokerEquity"] = pd.Series(broker._equity)
             self._results["Cash"]         = broker._cash
+            self._results["StrategyData"] = pd.DataFrame(indicator_attrs)
 
         return self._results
 
